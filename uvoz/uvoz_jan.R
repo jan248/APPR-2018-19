9a5400b60c416db37de44da1c646000ecbcb7e23
jan248 <jan.skoberne8@gmail.com>
2018-12-19 20:04:39 +0100

    3 tabele
    tidy data

--- uvoz/uvoz_jan.R
+++ uvoz/uvoz_jan.R
@@ -1,9 +1,15 @@
 library(dplyr)
 library(tidyr)
 
-# 1. tabela: brezposelnost po regijah
-brezposelnost.po.regijah<-read.csv2(file = 'podatki/Brezposelnost po regijah.csv', fileEncoding = 'Windows-1250', header = TRUE,
-                                    nrows = 9)
+
 # 2.tabela: stanovanjski standart
 stanovanjski.standard <- read.csv2(file = 'podatki/Stanovanjski standard.csv', fileEncoding = 'Windows-1250', header = FALSE,
-                                   skip = 4, nrows = 213,col.names = c('Regija', '2011', '2015'), na = c('', '-', ' ') )
+                                   skip = 4, nrows = 213,col.names = c('Regija', '2011', '2015'),sep = ';', dec = '.', na = c('', '-', ' ') )
+# 3.tabela: stanovanja v gradnji
+ocena.stanovanj.gradnja <- read.csv2(file = 'podatki/Ocena_stanovanj_v_gradnji.csv', fileEncoding = 'Windows-1250', header = FALSE,
+                                        skip = 4, nrows = 11, na = c('', '-', ' '), sep = ';', dec = '.',
+                                     col.names = c('oaknfoans','Leto','Stanovanja v gradnji - SKUPAJ','Stanovanja v gradnji - z začetkom gradnje v letu','Stanovanja v gradnji ob koncu leta','Dokončana stanovanja - SKUPAJ
+','Dokončana stanovanja - investitor pravna oseba','Dokončana stanovanja - investitor fizična oseba','Dokončana stanovanja na 1000 prebivalcev','baifba'))
+ocena.stanovanj.gradnja <- ocena.stanovanj.gradnja[,-1]
+ocena.stanovanj.gradnja <- ocena.stanovanj.gradnja[,-9]
+ocena.stanovanj.gradnja <- ocena.stanovanj.gradnja%>% filter(Leto != ' ')
